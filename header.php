d7349cf8e14e4a0371000fa5f0f646ffea5ab778
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2024-10-15 12:29:29 +0300

    add tailwind css and header from host2media next 3 project

--- header.php
+++ header.php
@@ -22,20 +22,108 @@
 <body <?php body_class(); ?>>
 
 <div id="page" class="site main_page_wrapper">
-	<script>
-		jQuery(document).ready(function($) {
-			fetch('https://ap.host2media.com/wp-json/api/v1/search-domain/', {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json',
-				},
-				body: JSON.stringify({
-					domain_name: 'host2media.com'
-				})
-			})
-			.then(response => response.json())
-			.then(data => console.log(data))
-			.catch(error => console.error('Error:', error));
-        });
-    </script>
 	<div id="content" class="site-content">
+	<div class="w-full">
+    <header>
+      <nav>
+        <div class="w-full py-3 bg-[#5564ad]">
+          <div class="grid grid-cols-12 gap-5">
+            <div class="col-span-6 hidden lg:block"></div>
+            <div class="col-span-12 lg:col-span-6">
+              <ul
+                class="flex lg:justify-end justify-center items-center text-white"
+              >
+                <li class="lg:hidden block sm:mr-8 mr-2">
+                  <NuxtLink to="/">
+                    <img class="h-6 w-12" src="<?php echo get_template_directory_uri(); ?>/inc/assets/images/logo.svg" />
+                  </NuxtLink>
+                </li>
+                <li class="mx-2 Mulish-light text-[0.75rem]">
+                  <a href="#"> Blog </a>
+                </li>
+                <li class="mx-2 Mulish-light text-[0.75rem]">
+                  <a href="#"> Knowladge Base </a>
+                </li>
+                <li class="mx-2 Mulish-light text-[0.75rem]">
+                  <a href="#"> Status </a>
+                </li>
+                <li
+                  class="mx-2 Mulish-light font-semibold text-[0.75rem] lg:pr-32"
+                >
+                  <a href="#"> Customer Portal </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </nav>
+      <nav
+        class="py-5 flex flex-row justify-start items-center lg:px-16 md:px-16 sm:px-8"
+      >
+        <NuxtLink to="/">
+            <img class="lg:block hidden h-12 w-18" src="<?php echo get_template_directory_uri(); ?>/inc/assets/images/logo.svg" />
+        </NuxtLink>
+        <ul class="basic-2/4 lg:flex hidden font-medium text-sm xl:text-lg pl-8">
+          <li class="mx-2">
+            <a class="" href="#"> Domains </a>
+          </li>
+          <li class="mx-2">
+            <a class="" href="#"> Website Hosting </a>
+          </li>
+          <li class="mx-2">
+            <a class="" href="#"> Professional Email </a>
+          </li>
+          <li class="mx-2">
+            <a class="" href="#"> Reseller Program </a>
+          </li>
+          <li class="mx-2">
+            <a class="" href="#"> Dev Serices </a>
+          </li>
+          <li class="mx-2">
+            <a class="" href="https://hostmedia.com/contact-us"> Contact us </a>
+          </li>
+        </ul>
+        <button
+          class="hamburger hamburger--collapse block lg:mr-0 mr-5 lg:hidden"
+          type="button"
+        >
+          <div class="menu_mobile_nav">
+            <div class="hamburger_menu_icon">
+              <div class="line bg-[#5564AD]"></div>
+              <div class="line bg-[#5564AD] middle_line"></div>
+              <div class="line bg-[#5564AD]"></div>
+            </div>
+          </div>
+        </button>
+        <div id="menu_mobile" class="menu_on_mobile block lg:hidden h-[100dvh]">
+          <div class="menu_on_mobile_wrapper h-[100dvh]">
+            <div class="menu_on_mobile_inner_wrapper relative flex justify-center items-center h-[100dvh]">
+              <div>
+                  <a class="block my-3 page_font animated_menu_el" >
+                      <img class="w-[5rem] mx-auto" src="<?php echo get_template_directory_uri(); ?>/inc/assets/images/h2m-mob.png" alt="host2media">
+                  </a>
+                  <a class="block my-3 page_font animated_menu_el" href="#">
+                    <div class="menu_item text-white">Domains</div>
+                  </a>
+                  <a class="block my-3 page_font animated_menu_el" href="#">
+                    <div class="menu_item text-white">Website Hosting</div>
+                  </a>
+                  <a class="block my-3 page_font animated_menu_el" href="#">
+                    <div class="menu_item text-white">Professional Email</div>
+                  </a>
+                  <a class="block my-3 page_font animated_menu_el" href="#">
+                    <div class="menu_item text-white">Reseller Program</div>
+                  </a>
+                  <a class="block my-3 page_font animated_menu_el" href="#">
+                    <div class="menu_item text-white">Dev Serices</div>
+                  </a>
+                  <a class="block my-3 page_font animated_menu_el">
+                    <div class="menu_item text-white">Contact US</div>
+                  </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </header>
+  </div>
