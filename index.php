0693bd64f8353908fed0117aae7943cf3e8603c6
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2024-10-23 23:39:14 +0300

    fix

--- index.php
+++ index.php
@@ -11,12 +11,12 @@ get_header();
 <section class="bg-[#FEFEFE]">
     <div class="container mx-auto">
         <div class="flex py-5">
-            <h3 class="text-black text-[1.25rem] Mulish-SemiBold font-bold" style="white-space: nowrap;">New Posts</h3>
+            <h3 class="text-black sm:text-[1.25rem] text-xs Mulish-SemiBold font-bold" style="white-space: nowrap;">New Posts</h3>
             <div class="border-custom"></div>
         </div>
-        <div class="grid grid-cols-12 gap-5 py-10 lg:mx-0 mx-5">
+        <div class="grid grid-cols-12 gap-5 py-10 lg:mx-0 mx-5 md:flex hidden">
             <div class="col-span-4 bg-[#FFF9F9] rounded-[10px] text-left">
-               <a href="#" class="bg-[#FFF9F9] custom-single-blog block h-full rounded-[10px]">
+                <a href="#" class="bg-[#FFF9F9] custom-single-blog block h-full rounded-[10px]">
                     <div class="sm:pb-10 pb-5">
                         <img
                             class="w-full"
@@ -78,6 +78,32 @@ get_header();
                     </div>
                 </a>
             </div>
+        </div>
+        <div class="swiper mainSwiper md:hidden flex">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide">
+                    <a href="#" class="bg-[#FFF9F9] custom-single-blog block h-full rounded-[10px]">
+                        <div class="sm:pb-10 pb-5">
+                            <img
+                                class="w-full"
+                                src="<?php echo get_template_directory_uri(); ?>/inc/assets/images/dedicated-server-bg.png"
+                                alt="dedicated-server"
+                            />
+                        </div>
+                        <div class="px-4">
+                            <p class="text-[#0F132A] Mulish-bold pb-4 text-lg">What Is a Dedicated Server? Understanding the Fundamentals</p>
+                            <p class="text-[#0F132A] Mulish-Regular pb-4 text-sm">Understanding the FundamentalsMuch like many organizations, you probably opted for cloud computing when you first launched your web application, database, or mail server...</p>
+                            <p class="mb-3 text-[#5564AD] Mulish-light text-xs block">
+                                7, September 2024
+                            </p>
+                            <p class="mb-3 text-[#5564AD] Mulish-light text-xs block">
+                                In Dedicated Server, Cloud Hybrid,
+                            </p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="swiper-pagination"></div>
         </div>
         <div class="flex py-5">
             <h3 class="text-black text-[1.25rem] Mulish-SemiBold font-bold w-[100px]" style="white-space: nowrap;">Old Posts</h3>
@@ -97,7 +123,7 @@ get_header();
                         </p>
                     </div>
                     <img
-                        class="w-full"
+                        class="w-full sm:block hidden"
                         src="<?php echo get_template_directory_uri(); ?>/inc/assets/images/dedicated-server-bg-2.png"
                         alt="dedicated-server"
                     />
@@ -206,5 +232,18 @@ get_header();
         </div>
     </div>
 </section>
+<script>
+    jQuery(document).ready(function($) {
+        var swiper = new Swiper(".mainSwiper", {
+            slidesPerView: 1.5,
+            spaceBetween: 20,
+            pagination: {
+                el: '.swiper-pagination',
+                type: 'bullets',
+                clickable: true,
+            },
+        });
+    })
+</script>
 <?php
 get_footer();
