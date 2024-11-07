5258ff92990b3105f26fbfb6ed26a5e4eccbc96b
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2024-11-08 00:45:12 +0200

    add social icons

--- single-post.php
+++ single-post.php
@@ -30,8 +30,9 @@ $title = get_the_title(get_the_ID());
                 <div class="col-span-3">
                     <div class="social-links">
                         <ul>
-                            <li class="block">
-                                <img class="w-6 h-6" src="<?php echo get_template_directory_uri(); ?>/inc/assets/icons/share-arrow.svg" alt="share-arrow">
+                            <li class="block relative" style="width: fit-content;">
+                                <img class="w-16" src="<?php echo get_template_directory_uri(); ?>/inc/assets/icons/share-arrow.svg" alt="share-arrow">
+                                <p class="text-white font-black text[12px] absolute right-0 bottom-0 left-0 top-0">share</p>
                             </li>
                         </ul>
                     </div>
