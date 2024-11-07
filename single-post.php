dd952820f602b551ee8981d7fc1c48474415d26e
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2024-11-07 17:27:52 +0200

    add single article page

--- single-post.php
+++ single-post.php
@@ -7,19 +7,23 @@
  * @package WP_Bootstrap_Starter
  */
 
-get_header(); ?>
+get_header();
+$post_title = urlencode(get_the_title(get_the_ID()));
+$post_image = wp_get_attachment_url(get_post_thumbnail_id(get_the_ID()));
+?>
 <div class="single-article-page">
     <section>
-        <img src="w-full" src="<?php echo wp_get_attachment_image_src(get_post_thumbnail_id(get_the_ID())) ?>" alt="<?php the_title(); ?>">
+        <img src="w-full" src="<?php echo $post_image; ?>" alt="<?php echo $post_title; ?>">
     </section>
-    <section>
-        <div class="container">
-            <div class="row text-left">
-                <div class="col-9">
-                    <div>
-                        <h1><?php the_title(); ?></h1>
-                        <?php the_content(); ?>
-                    </div>
+    <section class="py-5">
+        <div class="container mx-auto">
+            <div class="grid grid-cols-12 gap-5 py-10">
+                <div class="col-span-9">
+                    <h1><?php echo $post_title; ?></h1>
+                    <?php the_content(); ?>
+                </div>
+                <div class="col-span-3">
+
                 </div>
             </div>
         </div>
