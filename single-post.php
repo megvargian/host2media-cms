f40b30de4aba4e8e013cceebddda239e980ed4f2
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2024-11-07 17:44:21 +0200

    add single article page

--- single-post.php
+++ single-post.php
@@ -12,14 +12,14 @@ $post_title = urlencode(get_the_title(get_the_ID()));
 $post_image = wp_get_attachment_url(get_post_thumbnail_id(get_the_ID()));
 $title = the_title();
 ?>
-<div class="single-article-page">
+<div class="single-article-page bg-[#FEFEFE]">
     <section>
-        <img src="w-full" src="<?php echo get_the_post_thumbnail_url(get_the_ID()); ?>" alt="<?php echo $title; ?>">
+        <img class="w-full" src="<?php echo get_the_post_thumbnail_url(get_the_ID()); ?>" alt="<?php echo $title; ?>">
     </section>
     <section class="py-5">
         <div class="container mx-auto">
             <div class="grid grid-cols-12 py-10">
-                <div class="col-span-9">
+                <div class="col-span-9 text-left">
                     <h1><?php echo $title; ?></h1>
                     <?php the_content(); ?>
                 </div>
