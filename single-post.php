618729e40e232a6cec0a0debc79749f6d871e2dd
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2024-11-07 17:39:24 +0200

    add single article page

--- single-post.php
+++ single-post.php
@@ -14,11 +14,11 @@ $title = the_title();
 ?>
 <div class="single-article-page">
     <section>
-        <img src="w-full" src="<?php echo get_the_post_thumbnail(get_the_ID()); ?>" alt="<?php echo $title; ?>">
+        <img src="w-full" src="<?php echo get_the_post_thumbnail_url(get_the_ID()); ?>" alt="<?php echo $title; ?>">
     </section>
     <section class="py-5">
         <div class="container mx-auto">
-            <div class="grid grid-cols-12 gap-5 py-10">
+            <div class="grid grid-cols-12 py-10">
                 <div class="col-span-9">
                     <h1><?php echo $title; ?></h1>
                     <?php the_content(); ?>
